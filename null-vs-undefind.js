db97e88e84012e25f8ff8f570c25a9ee080a9fd2
MD Saifur Rahman <72568300+MDsaif777@users.noreply.github.com>
2021-04-25 12:50:33 +0600

    push code

---
+++ null-vs-undefind.js
@@ -0,0 +1,22 @@
+let phaki;
+
+function add(num1, num2){
+    console.log(num1+num2)
+    return
+}
+
+function add(num1, num2 ){
+    console.log(num1 , num2)
+}
+const result = add(12)
+
+const premik = {name: "smart dude", phone:43654};
+console.log(premik.gf)
+
+let fun = undefined;
+console.log(fun);
+
+let ages =[2, 4,6];
+console.log(ages[11]);
+
+
---
+++ true-false.js
@@ -0,0 +1,25 @@
+// falsy:
+// false
+// 0
+// ""
+// undefind
+// null
+// NaN
+
+// Truthy
+// '0', ' ',[], {}
+
+
+
+
+// const age = "0";
+
+let name =" ";
+
+if(name ){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false")
+}
