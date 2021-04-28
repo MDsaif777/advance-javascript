85b680eb8d76f5ee029289da2f2c7b95a1a0f778
MD Saifur Rahman <72568300+MDsaif777@users.noreply.github.com>
2021-04-28 12:28:30 +0600

    done

---
+++ array-split.js
@@ -0,0 +1,10 @@
+const nums =[1,2,3,4,5,6,7,8,9];
+const part = nums.slice(1,6);
+console.log(part)
+
+const remove = nums.splice(2,3,  77);
+console.log(remove);
+// console.log(nums);
+
+  const together = nums.join(' A');
+  console.log(together)
---
+++ scope.js
@@ -0,0 +1,22 @@
+let bonus = 20;
+
+function sum(first, secound){
+    let result = first+secound + bonus;
+    // console.log(bonus);
+    if(result > 6){
+        var mood ="happy";
+        mood ="fishy";
+        mood="funky";
+        mood="cranky";
+        // console.log(mood);
+    }
+    console.log(day);
+    let day ="Monday";
+   
+    
+    return result
+}
+const output = sum(3,4);
+// console.log(bonus)
+
+// console.log(output)
